228f76269344af16534b3ac966a100e39262b358
Elena-Erganyan <elena-erganyan@rambler.ru>
2016-12-24 20:47:35 +0400

    Написала автоматическую сборку проекта

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,31 @@ var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
 var precss = require("precss");
 var autoprefixer = require("autoprefixer");
+var mqpacker = require("css-mqpacker");
+var minify = require("gulp-csso");
+var rename = require("gulp-rename");
+var imagemin = require("gulp-imagemin");
+var svgstore = require("gulp-svgstore");
+var svgmin = require("gulp-svgmin");
+var del = require("del");
 var server = require("browser-sync").create();
+var run = require("run-sequence");
+
+gulp.task("clean", function() {
+  return del("build");
+});
+
+gulp.task("copy", function() {
+  return gulp.src([
+    "fonts/**/*.{woff,woff2}",
+    "img/**",
+    "js/**",
+    "*.html"
+  ], {
+    base: "."
+  })
+  .pipe(gulp.dest("build"));
+});
 
 gulp.task("style", function() {
   gulp.src("postcss/style.css")
@@ -14,15 +38,57 @@ gulp.task("style", function() {
       precss(),
       autoprefixer({browsers: [
         "last 2 versions"
-      ]})
+      ]}),
+      mqpacker({
+        sort: true
+      })
     ]))
-    .pipe(gulp.dest("css"))
-    .pipe(server.stream());
+    .pipe(gulp.dest("build/css"))
+    .pipe(server.stream())
+    .pipe(minify())
+    .pipe(rename("style.min.css"))
+    .pipe(gulp.dest("build/css"));
 });
 
-gulp.task("serve", ["style"], function() {
+gulp.task("images", function() {
+  return gulp.src("build/img/**/*.{png,jpg,gif}")
+    .pipe(imagemin([
+      imagemin.optipng({optimizationLevel: 3}),
+      imagemin.jpegtran({progressive: true})
+    ]))
+    .pipe(gulp.dest("build/img"));
+});
+
+gulp.task("symbols", function() {
+  return gulp.src ("build/img/**/*.svg")
+    .pipe(svgmin())
+    .pipe(gulp.dest("build/img"));
+});
+
+gulp.task("sprite", function() {
+  return gulp.src("build/img/icons/*.svg")
+    .pipe(svgstore({
+      inlineSvg: true
+    }))
+    .pipe(rename("sprite.svg"))
+    .pipe(gulp.dest("build/img/icons"));
+});
+
+gulp.task("build", function(fn) {
+  run(
+    "clean",
+    "copy",
+    "style",
+    "images",
+    "symbols",
+    "sprite",
+    fn
+  );
+});
+
+gulp.task("serve", function() {
   server.init({
-    server: ".",
+    server: "build",
     notify: false,
     open: true,
     cors: true,
